358841b74b563e9b5fe36555566cc9280aabde23
thedineshjoshi <9868459709dinesh@gmail.com>
2023-10-01 21:24:33 +0545

    New commit 09/01

--- C#/staticConstructor.cs
+++ C#/staticConstructor.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+public class Counter
+{
+    static int count;
+    string name;
+    string age;
+    string weight;
+    string guardian_name;
+        static Counter()
+    {
+        count = 0;
+        count++;
+    }
+    void get_data()
+    {
+        Console.Write("\t" + count);
+        Console.WriteLine("Enter the Name of Student: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Age = ");
+        age = Console.ReadLine();
+        Console.WriteLine("Weight = ");
+        weight = Console.ReadLine();
+        Console.WriteLine("Guardian Name = ");
+        guardian_name = Console.ReadLine();
+
+    }
+    static int Main()
+    {
+        Console.WriteLine("Enter the data of student");
+        Counter obj1 = new Counter();
+        obj1.get_data();
+        Counter obj2 = new Counter();
+        obj2.get_data();
+        Console.ReadKey();
+        return 0;
+    }
+
+}
