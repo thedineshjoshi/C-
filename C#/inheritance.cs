bce99f03250a7536b8b3b53afd88fbc46d8e2a5d
thedineshjoshi <9868459709dinesh@gmail.com>
2023-10-02 08:01:41 +0545

    inheritance added

--- C#/inheritance.cs
+++ C#/inheritance.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class numbers{
+    public int x = 10;
+    public int y = 20;
+}
+public class sum : numbers
+{
+    public int z;
+}
+public class test
+{
+    static int Main()    {
+
+        sum obj = new sum();
+         obj.z= obj.x+obj.y;
+        Console.WriteLine(obj.z);
+         
+        return 0;
+    }
+
+}
